6a5b3e178278ddaf49c5dcbb9891186fad1758fa
Stig B. Dørmænen <stigbd@gmail.com>
2023-03-01 12:38:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# azure-sample-spring-boot
+
+A small demo project to show how to use Azure Spring Boot Starter.
+
+## How to run
+
+```bash
+mvn clean spring-boot:run
+```
+
+## Play
+
+In your browser, open <http://localhost:8080/public> or <http://localhost:8080/user> or <http://localhost:8080/admin>
+
+You will need a valid Azure Active Directory account to access the protected resources.
+
+## Reference
+
+Based on [Add sign-in with Azure Active Directory account to a Spring web app](https://learn.microsoft.com/en-us/azure/developer/java/spring-framework/configure-spring-boot-starter-java-app-with-azure-active-directory)
---
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/public")
+    @ResponseBody
+    public String publicMethod() {
+        return "Hello public!";
+    }
+
+    @GetMapping("/user")
+    @ResponseBody
+    @PreAuthorize("hasAuthority('APPROLE_User')")
+    public String userMethod() {
+        return "Hello User!";
+    }
+
+    @GetMapping("/admin")
+    @ResponseBody
+    @PreAuthorize("hasAuthority('APPROLE_Admin')")
+    public String Admin() {
+        return "Hello Admin!";
+    }
+}
