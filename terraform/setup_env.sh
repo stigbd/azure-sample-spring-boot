a4914f61eaaa66367eb3f8fa4ba2693d0afc581e
Stig B. Dørmænen <stigbd@gmail.com>
2023-03-02 21:28:28 +0100

    Provision azure resource with terraform Fixes #1

---
+++ terraform/setup_env.sh
@@ -0,0 +1,22 @@
+export AZURE_TENANT_ID=$(terraform -chdir=./terraform output -raw AZURE_TENANT_ID)
+
+# set identifier_uris
+echo "----------update identifier-uris start----------"
+az ad app update --id $AZURE_CLIENT_ID --identifier-uris api://$AZURE_CLIENT_ID
+echo "----------update identifier-uris completed----------"
+
+export AZURE_CLIENT_ID=$(terraform -chdir=./terraform output -raw AZURE_CLIENT_ID)
+export SERVICE_PRINCIPAL=$(terraform -chdir=./terraform output -raw SERVICE_PRINCIPAL)
+export SERVICE_PRINCIPAL_PASSWORD=$(terraform -chdir=./terraform output -raw SERVICE_PRINCIPAL_PASSWORD)
+export USER_NAME=$(terraform -chdir=./terraform output -raw USER_NAME)
+export USER_PASSWORD=$(terraform -chdir=./terraform output -raw USER_PASSWORD)
+
+echo AZURE_CLIENT_ID=${AZURE_CLIENT_ID}
+echo SERVICE_PRINCIPAL_PASSWORD=${SERVICE_PRINCIPAL_PASSWORD}
+echo SERVICE_PRINCIPAL=${SERVICE_PRINCIPAL}
+echo AZURE_TENANT_ID=${AZURE_TENANT_ID}
+
+
+echo "--------created user--------"
+echo USER_NAME=${USER_NAME}
+echo USER_PASSWORD=${USER_PASSWORD}
