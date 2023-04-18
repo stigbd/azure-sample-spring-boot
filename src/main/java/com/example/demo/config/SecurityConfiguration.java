e45437f0d4bb5025f24edd588d7f62b7338b132c
Stig B. Dørmænen <stigbd@gmail.com>
2023-04-18 15:11:07 +0200

    feature: add probe endpoints

---
+++ src/main/java/com/example/demo/config/SecurityConfiguration.java
@@ -0,0 +1,15 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/actuator/**");
+    }
+
+}
